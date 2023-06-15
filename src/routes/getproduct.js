717caf78ade76ace8625b0f28dcfd1cca0726805
yuvaabs <absyuva@gmail.com>
2023-06-16 03:13:58 +0530

    auth added

--- src/routes/getproduct.js
+++ src/routes/getproduct.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const adminController = require('../controller/auth/adminController');
 
 const seeproduct=require('../controller/auth/seeproduct')
 
-router.get('/getproduct',seeproduct);
+router.get('/getproduct',adminController.approveadmin,seeproduct);
 
 module.exports=router;
